297bb740b772c094c53a00685a24a4fc91b65477
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-02-09 10:39:56 +0600

    Update linear_search.cpp

--- linear_search.cpp
+++ linear_search.cpp
@@ -11,27 +11,31 @@ int linear_search(int a[], int arraySize, int searchElement)
         {
             return i;
         }
+        else
+        {
+            i = -1;
+            return i;
+        }
     }
-    i = -1;
-    return i;
 }
 int main(int argc, char const *argv[])
 {
     int arr[] = {10, 12, 14, 15, 32, 56, 65, 34, 87, 90};
     int size = sizeof(arr) / sizeof(arr[0]);
-    cout << size << endl;
-    int search;
-    cout << "Search: ";
-    cin >> search;
+    // cout << size << endl;
+    int search = 14;
+    // cout << "Search: ";
+    // cin >> search;
     int searchResult = (arr, size, search);
-    if (searchResult != -1)
-    {
-        cout << "Your search element found at position: " << searchResult << endl;
-    }
-    else
-    {
-        cout << "Not found!" << endl;
-    }
+    cout << searchResult << endl;
+    // if (searchResult != -1)
+    // {
+    //     cout << "Your search element found at position: " << searchResult << endl;
+    // }
+    // else
+    // {
+    //     cout << "Not found!" << endl;
+    // }
 
     return 0;
 }

9977b4f7877d5f6ed5a258c9396734858c3b57fe
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-02-11 14:42:04 +0600

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 void selection_sort(int a[], int size)
 {
+    for (int i = 0; i < size - 1; i++)
+    {
+        int min_index = i;
+        for (int j = i + 1; i < size; i++)
+        {
+            if (a[j] < a[i])
+            {
+                min_index = j;
+            }
+        }
+        if (min_index != i)
+        {
+        }
+    }
 }
 
 int main(int argc, char const *argv[])

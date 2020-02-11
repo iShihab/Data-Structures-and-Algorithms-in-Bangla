edf2ca263e4cc6c2f5a772faf65e9261fb53311d
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-02-11 09:23:35 +0600

    Create selection_sort.cpp

---
+++ selection_sort.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void selection_sort(int a[], int size)
+{
+}
+
+int main(int argc, char const *argv[])
+{
+    int a[] = {10, 5, 2, 8, 7};
+    int size = 5;
+
+    return 0;
+}

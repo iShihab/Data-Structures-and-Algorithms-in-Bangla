5d4b11b283e830f4f1cf53cce8df47c6c348ae58
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-02-11 23:45:12 +0600

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -26,7 +26,7 @@ void selection_sort(int a[], int size)
 
 int main(int argc, char const *argv[])
 {
-    int arr[] = {8, 2, 14, 18, 14, 65};
+    int arr[] = {8, 2, 14, 18, 1, 65};
     int size = sizeof(arr) / sizeof(arr[0]);
     selection_sort(arr, size);
     for (int i = 0; i < size; i++)

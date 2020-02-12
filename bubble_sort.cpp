9603570efc0ad82275be070eca8c99053007bfc0
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-02-12 06:40:11 +0600

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+void bubble_sort(int a[], int size)
+{
+    int i, j, temp;
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size - 1 - i; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                temp = a[j];
+                a[j] = a[j + 1];
+            }
+        }
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     int a[] = {12, 14, 2, 42, 67, 4};

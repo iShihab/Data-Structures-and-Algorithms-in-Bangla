4a0dc2d5b91856b81d4fb9eb9d8597e6b0aca10e
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-02-13 12:11:15 +0600

    Create insertion_sort.cpp

---
+++ insertion_sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void insertion_sort(int a[], int size)
+{
+    int i, j, item;
+    for (i = 1; i < size; i++)
+    {
+        int item = a[i];
+        j = i - 1;
+        while (j >= 0 && a[j] > item)
+        {
+            a[j + 1] = a[j];
+            j = j - 1;
+        }
+    }
+    a[j + 1] = item;
+}
+
+int main(int argc, char const *argv[])
+{
+
+    int a[] = {53, 21, 34, 22, 54};
+    int size = sizeof(a) / sizeof(a[0]);
+    insertion_sort(a, size);
+    for (int i = 0; i < size; i++)
+    {
+        cout << "sorted array: " << a[i] << " ";
+    }
+
+    return 0;
+}

ea88a98af7ba594ea7be751e45f3682b1a4c2206
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-02-15 12:37:18 +0600

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -4,30 +4,27 @@ using namespace std;
 
 void insertion_sort(int a[], int size)
 {
-    int i, j, item;
+    int i, j, temp;
     for (i = 1; i < size; i++)
     {
-        item = a[i];
         j = i - 1;
-        while (j >= 0 && a[j] > item)
+        while (j >= 0)
         {
-            a[j + 1] = a[j];
-            j = j - 1;
+            if (a[i] > a[j])
+            {
+                temp = a[j];
+                a[j] = a[i];
+                a[i] = temp;
+            }
+            j--;
         }
     }
-    a[j + 1] = item;
 }
 
 int main(int argc, char const *argv[])
 {
-
-    int a[] = {5, 4, 2, 5, 1};
+    int a[] = {12, 11, 13, 5, 6};
     int size = sizeof(a) / sizeof(a[0]);
-    insertion_sort(a, size);
-    for (int i = 0; i < size; i++)
-    {
-        cout << a[i] << " ";
-    }
 
     return 0;
 }

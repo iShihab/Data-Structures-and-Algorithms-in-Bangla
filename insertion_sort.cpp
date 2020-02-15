3ad11ba27426b5d988e305fda843d5d44c6885a4
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-02-15 13:12:58 +0600

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -10,7 +10,7 @@ void insertion_sort(int a[], int size)
         j = i - 1;
         while (j >= 0)
         {
-            if (a[i] > a[j])
+            if (a[i] < a[j])
             {
                 temp = a[j];
                 a[j] = a[i];
@@ -25,6 +25,11 @@ int main(int argc, char const *argv[])
 {
     int a[] = {12, 11, 13, 5, 6};
     int size = sizeof(a) / sizeof(a[0]);
+    insertion_sort(a, size);
+    for (int i = 0; i < size; i++)
+    {
+        cout << a[i] << " ";
+    }
 
     return 0;
 }

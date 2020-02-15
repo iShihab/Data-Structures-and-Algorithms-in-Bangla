2b4ecb184a5d910f5774873e363edce65a389846
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-02-15 17:36:36 +0600

    Create Test1.cpp

---
+++ Test1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a, b, c;
+    a = 0, b = 0;
+    c = (a + b) / 2;
+    cout << c;
+    return 0;
+}

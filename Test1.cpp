20ece42edb85732618baebf5e26c7cedd009a714
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-02-19 20:33:32 +0600

    Update Test1.cpp

--- Test1.cpp
+++ Test1.cpp
@@ -2,11 +2,31 @@
 
 using namespace std;
 
+void insertion_sort(int a[], int size)
+{
+    int i, j, key;
+    for (i = 1; i < size; i++)
+    {
+        key = a[i];
+        j = i - 1;
+        while (j >= 0 && a[j] > key)
+        {
+            a[j + 1] = a[j];
+            j = j - 1;
+        }
+        a[j + 1] = key;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
-    int a, b, c;
-    a = 0, b = 0;
-    c = (a + b) / 2;
-    cout << c;
+    int a[] = {12, 11, 13, 5, 6};
+    int size = sizeof(a) / sizeof(a[0]);
+    insertion_sort(a, size);
+    for (int i = 0; i < size; i++)
+    {
+        cout << a[i] << " ";
+    }
+
     return 0;
 }
